21e91854b9a68fa415971725f9af80f95b320727
sleephu <sleephu2@gmail.com>
2014-04-15 09:17:23 -0400

    Initial Commit

---
+++ Final/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Final
+//
+//  Created by sleephu on 4/15/14.
+//  Copyright (c) 2014 Jingyun Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Final/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Final
+//
+//  Created by sleephu on 4/15/14.
+//  Copyright (c) 2014 Jingyun Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
